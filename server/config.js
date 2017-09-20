bbb6c0d40ea1a938a46bbeab869a9ac7be6337f8
jeffreynerona <jeffnerona@gmail.com>
2017-09-20 13:17:44 +0800

    added mongodb

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 const config = {
-  mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/mern-starter',
+  mongoURL: process.env.MONGO_URL || 'mongodb://jeffreynerona:VyxfpsiDdWh4oF1I@cluster0-shard-00-00-k7flg.mongodb.net:27017,cluster0-shard-00-01-k7flg.mongodb.net:27017,cluster0-shard-00-02-k7flg.mongodb.net:27017/mernstarter2?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin',
   port: process.env.PORT || 8000,
 };
 
